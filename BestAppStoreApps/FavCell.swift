66aaa0b61e5c59faaea4bc6f282db745f94ab5c8
guy twig <guy.twig@greenco.tech>
2022-04-27 00:08:29 +0300

    Created a favorite list

---
+++ BestAppStoreApps/FavCell.swift
@@ -0,0 +1,35 @@
+//
+//  FavCell.swift
+//  BestAppStoreApps
+//
+//  Created by Guy Twig on 26/04/2022.
+//
+
+import UIKit
+
+protocol FavCellDelegate {
+    func updateFavArray(favArray: [Results])
+}
+
+class FavCell: UICollectionViewCell {
+
+    static let nibName = "FavCell"
+    var favArray: [Results] = []
+    var index = Int()
+    var delegate: FavCellDelegate?
+    
+    @IBOutlet weak var appName: UILabel!
+    @IBOutlet weak var appImageView: UIImageView!
+    
+    
+    func updateCellContent() {
+        appName.text = favArray[index].name
+        appImageView.image = UIImage(url: URL(string: favArray[index].artworkUrl100))
+    }
+    
+    
+    @IBAction func remove(_ sender: UIButton) {
+        favArray.remove(at: index)
+        delegate?.updateFavArray(favArray: favArray)
+    }
+}
--- BestAppStoreApps/FavoritesViewController.swift
+++ BestAppStoreApps/FavoritesViewController.swift
@@ -11,16 +11,50 @@ import UIKit
 class FavoritesViewController: UIViewController {
     
     var favArray: [Results] = []
+    @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(UINib(nibName: FavCell.nibName, bundle: nil), forCellWithReuseIdentifier: FavCell.nibName)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
         if let fav = UserDefaults.standard.favListSave {
             favArray = fav
+            collectionView.reloadData()
         }
     }
 }
+
+extension FavoritesViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        favArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FavCell.nibName, for: indexPath) as! FavCell
+        cell.favArray = favArray
+        cell.index = indexPath.row
+        cell.delegate = self
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (view.frame.width) / 2.3
+        return CGSize(width: width, height: width)
+    }
+}
+
+extension FavoritesViewController: FavCellDelegate {
+    func updateFavArray(favArray: [Results]) {
+        self.favArray = favArray
+        UserDefaults.standard.favListSave = self.favArray
+        collectionView.reloadData()
+    }
+}
+
+
